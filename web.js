0726a2b113544eb27277928d0ef4f848c21626dd
Paul Chavarria <pjchavarria@gmail.com>
2013-07-12 01:54:15 +0000

    Edited again hehe

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 app.get('/', function(request, response) {
-  var response = rs.readFileSync('index.html','utf8');
-  response.send(response);
+  var resp = fs.readFileSync('index.html','utf8');
+  response.send(resp);
 });
 
 var port = process.env.PORT || 5000;
